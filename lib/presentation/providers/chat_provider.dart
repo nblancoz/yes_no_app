c079f5e0c876b43f0331ba3f99be711d30390f17
nblancoz <nblancozappala@gmail.com>
2024-07-30 18:56:25 +0200

    added provider & domain folder

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messager = [
+    Message(text: 'Hola Cris', fromWho: FromWho.me),
+    Message(text: 'Volverias al Madrid?', fromWho: FromWho.me)
+  ];
+
+  Future<void> sendMessage(String text) async {
+    // todo
+  }
+}
